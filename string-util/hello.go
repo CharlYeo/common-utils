6136918fa7e410a0c1c07acea3dbf6156755969d
887202 <qy995115399@qq.com>
2023-05-03 15:50:35 +0800

    update for test

--- string-util/hello.go
+++ string-util/hello.go
@@ -1,1 +1,8 @@
 package string_util
+
+import "fmt"
+
+func Hello() string {
+	fmt.Println("---> kunStyle: hello~,练习时长两年半")
+	return "hello"
+}
--- string-util/hello_test.go
+++ string-util/hello_test.go
@@ -1,1 +1,11 @@
 package string_util
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	resp := Hello()
+	fmt.Println("resp is :", resp)
+}
